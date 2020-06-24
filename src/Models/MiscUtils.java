bac0174b7d2929457adea1481b0afcab8a52aa59
Ahmad Waleed <a.waleedothman@gmail.com>
2020-06-24 12:13:20 +0200

    started working on slicing fruits

--- src/Models/MiscUtils.java
+++ src/Models/MiscUtils.java
@@ -1,10 +1,43 @@
 package Models;
-import java.lang.Math;
+
+import java.awt.*;
+import java.awt.geom.AffineTransform;
+import java.awt.image.AffineTransformOp;
+import java.awt.image.BufferedImage;
 
 public class MiscUtils {
-	public static double rand(double min, double max) {
+    public static double rand(double min, double max) {
         double range = max - min;
         double rand = (Math.random() * range) + min;
         return rand;
-	}
+    }
+
+    public static BufferedImage rotateRight(BufferedImage bufferedImage, double angle) {
+        AffineTransform transform = new AffineTransform();
+        transform.rotate(angle, 0, bufferedImage.getHeight());
+        AffineTransformOp op = new AffineTransformOp(transform, AffineTransformOp.TYPE_BILINEAR);
+        return op.filter(bufferedImage, null);
+    }
+
+    public static BufferedImage rotateLeft(BufferedImage bufferedImage, double angle) {
+        AffineTransform transform = new AffineTransform();
+        transform.rotate(-angle, bufferedImage.getWidth(), bufferedImage.getHeight());
+        AffineTransformOp op = new AffineTransformOp(transform, AffineTransformOp.TYPE_BILINEAR);
+        return op.filter(bufferedImage, null);
+    }
+
+    public static BufferedImage concat(BufferedImage img1, BufferedImage img2) {
+
+        //do some calculate first
+        int wid = img1.getWidth() + img2.getWidth();
+        int height = Math.max(img1.getHeight(), img2.getHeight());
+        //create a new buffer and draw two image into the new image
+        BufferedImage newImage = new BufferedImage(wid, height, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2 = newImage.createGraphics();
+        //draw image
+        g2.drawImage(img1, null, 0, 0);
+        g2.drawImage(img2, null, img1.getWidth(), 0);
+        g2.dispose();
+        return newImage;
+    }
 }
---
+++ src/Test/Game2.java
@@ -0,0 +1,40 @@
+package Test;
+
+import Models.MiscUtils;
+import javafx.application.Application;
+import javafx.embed.swing.SwingFXUtils;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+public class Game2 extends Application {
+
+    public static void main(String[] args) {
+        launch();
+
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Pane root = new Pane();
+        BufferedImage right = ImageIO.read(new File("src/resources/fruits/Watermelon_right.png"));
+        BufferedImage left = ImageIO.read(new File("src/resources/fruits/Watermelon_left.png"));
+        for (int i = 0; i < 3; i++) {
+            right = MiscUtils.rotateRight(right, Math.PI / 20);
+            left = MiscUtils.rotateLeft(left, Math.PI / 20);
+        }
+        root.setStyle("-fx-background-color: black;");
+        Image image = SwingFXUtils.toFXImage(MiscUtils.concat(left, right), null);
+        ImageView imageView = new ImageView(image);
+        root.getChildren().addAll(imageView);
+        Scene scene = new Scene(root, 1000, 500);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
