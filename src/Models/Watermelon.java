aafd20ad2048fce2837522f41e9264bc26bd5853
asmaaramadan99 <58226647+asmaaramadan99@users.noreply.github.com>
2020-06-28 16:56:36 +0200

    image view added

--- src/Models/Watermelon.java
+++ src/Models/Watermelon.java
@@ -1,6 +1,11 @@
 package Models;
 
 import javax.imageio.ImageIO;
+
+import javafx.embed.swing.SwingFXUtils;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
 import java.io.File;
 import java.io.IOException;
 
@@ -10,6 +15,10 @@ public class Watermelon extends Fruit {
             this.image = ImageIO.read(new File("src/resources/fruits/watermelon.png"));
             this.image_left = ImageIO.read(new File("src/resources/fruits/watermelon_left.png"));
             this.image_right = ImageIO.read(new File("src/resources/fruits/watermelon_right.png"));
+            this.currentView=new ImageView( SwingFXUtils.toFXImage(this.image, null));
+            this.currentView.setOnMouseEntered(MiscUtils.assignListener(this));
+            
+            
         } catch (IOException e) {
             MiscUtils.fileNotFound();
         }
