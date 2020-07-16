f7448e63e1db45cbbad6d92576693743eebf65ea
asmaaramadan99 <58226647+asmaaramadan99@users.noreply.github.com>
2020-07-16 02:02:00 +0200

    speed factor added

--- src/Models/Equation.java
+++ src/Models/Equation.java
@@ -19,8 +19,8 @@ public class Equation {
 
 	public Point updateCoordinates(int x, int y, double deltaTime) {
 		Point point = new Point();
-		point.x = (int) (initialSpeed * deltaTime * Math.cos(projectionAngle) / 1000 + startPoint);
-		point.y = (int) (initialSpeed * deltaTime * Math.sin(projectionAngle) / 1000
+		point.x = (int) (initialSpeed * deltaTime*GameLogic.speedFactor * Math.cos(projectionAngle) / 1000 + startPoint);
+		point.y = (int) (initialSpeed * deltaTime*GameLogic.speedFactor * Math.sin(projectionAngle) / 1000
 				- 0.5 * 9.8 * deltaTime * deltaTime / 1000000);
 		point.y = this.screenHeight - point.y;
 		return point;
